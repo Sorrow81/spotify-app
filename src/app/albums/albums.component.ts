ad55ee3857065764e33d321452c904f74520db29
Sorrow <seekcoldgirl@gmail.com>
2020-06-05 14:48:15 +0200

    message service

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AlbumService} from '../service/album.service';
 import {Track} from "../models/track.model";
+import {MessageService} from "../service/message.service";
 
 @Component({
   selector: 'app-albums',
@@ -14,11 +15,17 @@ export class AlbumsComponent implements OnInit {
 
   selectedTrack: Track;
 
-  constructor(private albumService: AlbumService) {
+  constructor(private albumService: AlbumService,
+              private messageService: MessageService) {
   }
 
   ngOnInit(): void {
     this.getTracks();
+  }
+
+  onSelect(track: Track): void {
+    this.selectedTrack = track;
+    this.messageService.add(`AlbumService: Selected track track_number=${track.track_number}`);
   }
 
   getTracks(): void {
@@ -27,9 +34,5 @@ export class AlbumsComponent implements OnInit {
         this.title = data['name'];
         this.tracks = data['tracks']['items'];
       });
-  }
-
-  onSelect(track: Track): void {
-    this.selectedTrack = track;
   }
 }
--- src/app/service/album.service.ts
+++ src/app/service/album.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Album} from "../models/album.model";
+import { MessageService } from './message.service';
+import {Track} from "../models/track.model";
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +13,12 @@ export class AlbumService {
   private oAuthToken = 'BQAImTEtgHitZ327OSrrLCa1aCGUOPdho4atFyAUgbc9LtdVkiPvBO9t9ETrf4fK_XkAU3pNU0Rm-ww9xZb_m9JMJix_dopjvxlOhyXmAvrAFFbKwK0yOrSXWAKFvmOyw8d9jTBtOmXu'; // https://developer.spotify.com/console/get-album/?id=0sNOF9WDwhWunNAHPD3Baj&market=FR
 
   constructor(
-    private http: HttpClient) {
+    private http: HttpClient,
+    private messageService: MessageService) {
   }
 
   getTracks(): Observable<Album[]> {
+    this.messageService.add('AlbumService: fetched tracks');
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + this.oAuthToken
